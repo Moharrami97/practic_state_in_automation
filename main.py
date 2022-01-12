3fb1a8588a8b3badc7290930fb74e6276f938527
Narges <narges.moharrami97@gmail.com>
2022-01-11 16:16:12 -0800

    commit started. classes added

---
+++ main.py
@@ -0,0 +1,25 @@
+# I want to send a letter in a hierarchical manner from the client to the manager.
+# this is an exercise to learn the state design pattern.
+
+class Message:
+    pass
+
+
+class ManagingDirector:
+    pass
+
+
+class InternalManger:
+    pass
+
+
+class Supervisor:
+    pass
+
+
+class Operator:
+    pass
+
+
+class Client:
+    pass
