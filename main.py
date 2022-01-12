238c3400bb68a4daa2a58a1af77822347fd73243
Narges <narges.moharrami97@gmail.com>
2022-01-11 16:48:21 -0800

    determine the access of individuals

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 # I want to send a letter in a hierarchical manner from the client to the manager.
 # this is an exercise to learn the state design pattern.
+from abc import ABC, abstractmethod
+
 
 class Message:
-    pass
+    def __init__(self, subject, body, sender):
+        self.subject = subject
+        self.body = body
+        self.sender = sender
+        self.flow = [sender]
+
+    def send(self, to_use):
+        pass
 
 
-class ManagingDirector:
-    pass
+class AbstractUSer(ABC):
+    @property
+    @abstractmethod
+    def allowed(self):
+        pass
 
 
-class InternalManger:
-    pass
+class ManagingDirector(AbstractUSer):
+    allowed = []
 
 
-class Supervisor:
-    pass
+class InternalManger(AbstractUSer):
+    allowed = [ManagingDirector]
 
 
-class Operator:
-    pass
+class Supervisor(AbstractUSer):
+    allowed = [InternalManger]
 
 
-class Client:
-    pass
+class Operator(AbstractUSer):
+    allowed = [Supervisor]
+
+
+class Client(AbstractUSer):
+    allowed = [Operator]
+
+
+if __name__ == "__main__":
+    opt = Operator()
+    spr = Supervisor()
+    inm = InternalManger()
+    mnd = ManagingDirector()
+    client = Client()
+
+    message = Message("Issue #123", "Issue description", client)
